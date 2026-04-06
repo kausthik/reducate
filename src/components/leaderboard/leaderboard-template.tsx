3911b8d3fdc5cc98583c85fa0b119ce2cfc3a13d
Kaushal Kumar <kaushal.cse.23@nitap.ac.in>
2026-04-06 19:45:14 +0530

    feat: update leaderboard layout for improved responsiveness on different screen sizes

--- src/components/leaderboard/leaderboard-template.tsx
+++ src/components/leaderboard/leaderboard-template.tsx
@@ -30,7 +30,7 @@ export function Leaderboard({ category, data }: Props) {
         🏆 Leaderboard
       </h1>
 
-      <div className="grid grid-cols-3 gap-4 mb-8">
+      <div className="grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-8">
         {topThree.map((user, index) => (
           <div
             key={user.username}
